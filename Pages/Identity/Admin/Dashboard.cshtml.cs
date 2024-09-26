05c0e66331c07df8c7c5ad0a5765b6ce8124a2b3
Bilal Khalid <bilal.khalid@teo-intl.com>
2024-09-26 13:15:32 +0500

    setup base structure for admin and user workflows

---
+++ Pages/Identity/Admin/Dashboard.cshtml.cs
@@ -0,0 +1,10 @@
+namespace IdentityApp.Pages.Identity.Admin
+{
+    public class DashboardModel : AdminPageModel
+    {
+        public int UsersCount { get; set; } = 0;
+        public int UsersUnconfirmed { get; set; } = 0;
+        public int UsersLockedout { get; set; } = 0;
+        public int UsersTwoFactor { get; set; } = 0;
+    }
+}
