3df2ca60d9030354bbf659d13a73b6d49a81f011
Bilal Khalid <bilal.khalid@teo-intl.com>
2024-09-26 14:00:22 +0500

    update onget and onpost methods from identity api

--- Pages/Identity/Admin/Dashboard.cshtml.cs
+++ Pages/Identity/Admin/Dashboard.cshtml.cs
@@ -1,10 +1,49 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace IdentityApp.Pages.Identity.Admin
 {
     public class DashboardModel : AdminPageModel
     {
+        public DashboardModel(UserManager<IdentityUser> usrMgr) => UserManager = usrMgr;
+        public UserManager<IdentityUser> UserManager { get; set; }
         public int UsersCount { get; set; } = 0;
         public int UsersUnconfirmed { get; set; } = 0;
         public int UsersLockedout { get; set; } = 0;
         public int UsersTwoFactor { get; set; } = 0;
+
+        private readonly string[] emails = {
+            "alice@example.com", "bob@example.com", "charlie@example.com"
+        };
+
+        public void OnGet()
+        {
+            UsersCount = UserManager.Users.Count();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            foreach (IdentityUser existingUser in UserManager.Users.ToList())
+            {
+                IdentityResult result = await UserManager.DeleteAsync(existingUser);
+                result.Process(ModelState);
+            }
+            foreach (string email in emails)
+            {
+                IdentityUser userObject = new IdentityUser
+                {
+                    UserName = email,
+                    Email = email,
+                    EmailConfirmed = true
+                };
+                var result = await UserManager.CreateAsync(userObject);
+                result.Process(ModelState);
+            }
+            if(ModelState.IsValid)
+                return RedirectToPage();
+            return Page();
+        }
     }
 }
