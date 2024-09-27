2fc2766d4d16282a447d6ca7a4dedaf509eb1e7e
Bilal Khalid <bilal.khalid@teo-intl.com>
2024-09-27 22:42:55 +0500

    add password recovery for user

--- Services/IdentityEmailService.cs
+++ Services/IdentityEmailService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Routing;
@@ -28,6 +29,14 @@ namespace IdentityApp.Services
             string safeToken = TokenEncoder.EncodeToken(token);
             return LinkGenerator.GetUriByPage(ContextAccessor.HttpContext, page,
                 null, new { email = emailAddress, token = safeToken });
+        }
+        public async Task SendPasswordRecoveryEmail(IdentityUser user,
+            string confirmationPage)
+        {
+            string token = await UserManager.GeneratePasswordResetTokenAsync(user);
+            string url = GetUrl(user.Email, token, confirmationPage);
+            await EmailSender.SendEmailAsync(user.Email, "Set Your Password",
+                $"Please set your password by <a href={url}>clicking here</a>.");
         }
     }
 }
