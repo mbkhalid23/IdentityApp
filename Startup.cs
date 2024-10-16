8870d21d8b105a477c6ad2226f4357c54226f6e8
Bilal Khalid <bilal.khalid@teo-intl.com>
2024-10-16 11:06:57 +0500

    implement client side authentication for apis along with returning proper status codes for apis

--- Startup.cs
+++ Startup.cs
@@ -77,6 +77,7 @@ namespace IdentityApp {
                 opts.LoginPath = "/Identity/SignIn";
                 opts.LogoutPath = "/Identity/SignOut";
                 opts.AccessDeniedPath = "/Identity/Forbidden";
+                opts.Events.DisableRedirectionForApiClients();
             });
         }
 
